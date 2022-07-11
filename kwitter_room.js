cd57f16e63e6a6b3fb9c18c9cc2d908ddbe8803a
EAP2622 <95591203+EAP2622@users.noreply.github.com>
2022-07-11 16:14:31 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,3 +10,23 @@ var firebaseConfig = {
   
   // Initialize Firebase
   var app = initializeApp(firebaseConfig);
+
+  function addRoom() {
+    room_name=document.getElementById("room_name").value;
+    localStorage.setItem("room_name",room_name);
+    window.location="kwitter_room.html";
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+"<div> room_name.value </div>"
+//End code
+});});}
+getData();
+
+function redirect() {
+  localStorage.setItem("room_name", room_name);
+    window.location="kwitter_room.html";
+}
